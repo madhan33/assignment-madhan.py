f0b51ae7de46837f9bbb2acbbba50f1aea311bfd
MADHAN <91709876+madhan33@users.noreply.github.com>
2021-10-12 05:36:52 -0400

    Add files via upload

---
+++ ass-0.py
@@ -0,0 +1,34 @@
+
+
+
+#assignment operators
+
+ 
+  
+x=int (input("type input value of X: "))
+print("the x is = ",x)
+
+y=int (input("type input value of y: "))
+print("the y is = ",y)
+
+print("the sum of = ",x+y)
+
+
+
+
+A=int(input("type a value of A: "))
+print("the A value= ",A)
+
+
+B=20
+print("the B is = ",B)
+print("the A-B= ",A-B)
+
+B=2
+print("the B value= ",B)
+C=5
+print("the C value= ",C)
+print("the sum of B*C= ",B*C)
+
+
+
